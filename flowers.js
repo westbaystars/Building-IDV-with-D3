e6e52b3ca1893e003879bf157a6ef33c16d0abc5
Packt <packt@FirstBase.local>
2015-06-20 20:21:53 +0900

    Initial flower data file.

---
+++ flowers.js
@@ -0,0 +1,34 @@
+// Data
+
+var flowers = [
+  {
+    "name": "Hydrangia",
+    "latin": "Hydrangea",
+    "light": "Sun",
+    "ease": 3
+  },
+  {
+    "name": "Chrysanthemum",
+    "latin": "Chrysanthemum",
+    "light": "Part Sun",
+    "ease": 1
+  },
+  {
+    "name": "Peony",
+    "latin": "Paeonia",
+    "light": "Part Sun",
+    "ease": -1
+  },
+  {
+    "name": "Orchid",
+    "latin": "Paeonia",
+    "light": "Shade",
+    "ease": -5
+  },
+  {
+    "name": "Fern",
+    "latin": "Dryopteris",
+    "light": "Shade",
+    "ease": 5
+  }
+];
